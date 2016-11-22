3b27cfa84b685ee725d98a179ebbd5c2ed96021f
Emanuel Umbelino <emanuel.umbelin@gmail.com>
2016-11-22 12:30:45 -0200

    Version 1.5
    First server tests

--- app/src/main/java/nave/com/test/MainActivity.java
+++ app/src/main/java/nave/com/test/MainActivity.java
@@ -1,7 +1,11 @@
 package nave.com.test;
 
+import android.content.ComponentName;
 import android.content.Context;
-import android.content.res.AssetManager;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Handler;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -10,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -22,30 +27,158 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ServiceConnection, Runnable
+{
 
     private EditText email;
     private EditText password;
     private EditText confirm ;
+    private EditText name ;
     private EditText emailBack;
     private EditText passwordBack ;
     private EditText confirmBack ;
+    private EditText nameBack;
     private TextView messager;
     private boolean sign ;
+
+    private HTTPService myService;
+    private HTTPRequests.Services currService;
+    private final ServiceConnection connection = this;
+    private Intent i;
+    private Handler handler;
+    private Button buttonLogin;
+    private int pingTime;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SetInterface();
+
+        i = new Intent(this, HTTPService.class);
+
+        pingTime = 0;
+
+        handler = new Handler();
+        startService(i);
+
+        buttonLogin = (Button) findViewById(R.id.logButton);
+        buttonLogin.setVisibility(View.INVISIBLE);
+        ShowMessage("No Connection", false);
+
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        bindService(i, connection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        handler.removeCallbacks(this);
+
+        if(myService.GetRequests() != null)
+        {
+            unbindService(connection);
+            stopService(i);
+        }
+    }
+
+    private void Login(String login, String pass)
+    {
+        myService.GetRequests().Login(login, pass);
+        currService = HTTPRequests.Services.LOGIN;
+        pingTime = 0;
+        handler.post(this);
+    }
+
+    @Override
+    public void onServiceConnected(ComponentName name, IBinder service)
+    {
+        HTTPService.HTTPBinder binder = (HTTPService.HTTPBinder) service;
+        myService = binder.GetService();
+        myService.GetRequests().Connect();
+        currService = HTTPRequests.Services.CONNECT;
+        pingTime = 0;
+        handler.post(this);
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName name)
+    {
+        myService = null;
+        handler.removeCallbacks(this);
+    }
+
+    @Override
+    public void run()
+    {
+        if(myService.GetRequests().ServerResponse() != null)
+        {
+            if(currService == HTTPRequests.Services.CONNECT) ConnectionResponse(myService.GetRequests().ServerResponse());
+            else if (currService == HTTPRequests.Services.LOGIN) LoginResponse(myService.GetRequests().ServerResponse());
+        }
+        else
+        {
+            if(pingTime < 5)
+            {
+                pingTime++;
+                handler.postDelayed(this, 3000);
+                Toast.makeText(this, "Waiting server response - Attempt: " + pingTime, Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                pingTime = 0;
+                Toast.makeText(this, "Server doesn't response. Try again later.", Toast.LENGTH_SHORT).show();
+                finish(); //backing to previous Activity
+            }
+        }
+
+        myService.GetRequests().NullServerResponse();
+    }
+
+    private void ConnectionResponse(Object serverResponse)
+    {
+        if((Boolean) serverResponse)
+        {
+            Toast.makeText(this, "Server connected!!", Toast.LENGTH_SHORT).show();
+            buttonLogin.setEnabled(true);
+            ShowMessage("  ", false);
+        } else
+        {
+            unbindService(connection);
+            stopService(i);
+
+            Toast.makeText(this, "Problems to connect with server", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void LoginResponse(Object serverResponse) {
+        if(!serverResponse.toString().equals(""))
+            ShowMessage(serverResponse.toString(), true);
+        else
+            ShowMessage("User not found", true);
+    }
+
+    private void SetInterface()
+    {
         sign = false;
         emailBack = getEditText("editText");
         passwordBack = getEditText("editText2");
         confirmBack = getEditText("editText3");
+        nameBack = getEditText("editText5");
         emailBack.setKeyListener(null);
         passwordBack.setKeyListener(null);
         confirmBack.setKeyListener(null);
         email = getEditText("emailText");
         password = getEditText("passText");
         confirm = getEditText("confirmText");
+        name = getEditText("name");
         messager = (TextView)findViewById(R.id.message);
         email.addTextChangedListener(new TextWatcher()
         {
@@ -95,8 +228,23 @@ public class MainActivity extends AppCompatActivity {
 
             public void onTextChanged(CharSequence s, int start, int before, int count) {}
         });
-    }
+        name.addTextChangedListener(new TextWatcher()
+        {
+            public void afterTextChanged(Editable s)
+            {
+                if(isEmpty(name))
+                {
+                    nameBack.setText(R.string.Name);
+                }
+                else
+                    nameBack.setText("  ");
+            }
 
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+        });
+    }
     private EditText getEditText(String id)
     {
         int resID = getResources().getIdentifier(id, "id", "nave.com.test");
@@ -113,6 +261,10 @@ public class MainActivity extends AppCompatActivity {
             s.setVisibility(View.VISIBLE);
             s = (EditText) findViewById(R.id.confirmText);
             s.setVisibility(View.VISIBLE);
+            s = (EditText) findViewById(R.id.editText5);
+            s.setVisibility(View.VISIBLE);
+            s = (EditText) findViewById(R.id.name);
+            s.setVisibility(View.VISIBLE);
             Button b = (Button)  findViewById(R.id.signButton);
             b.setVisibility(View.VISIBLE);
             b = (Button)  findViewById(R.id.logButton);
@@ -126,6 +278,10 @@ public class MainActivity extends AppCompatActivity {
             EditText s = (EditText) findViewById(R.id.editText3);
             s.setVisibility(View.GONE);
             s = (EditText) findViewById(R.id.confirmText);
+            s.setVisibility(View.GONE);
+            s = (EditText) findViewById(R.id.editText5);
+            s.setVisibility(View.GONE);
+            s = (EditText) findViewById(R.id.name);
             s.setVisibility(View.GONE);
             Button b = (Button)  findViewById(R.id.signButton);
             b.setVisibility(View.GONE);
@@ -153,24 +309,31 @@ public class MainActivity extends AppCompatActivity {
 
     public void SaveLogin(View v)
     {
-        if(!isEmpty(email) && !isEmpty(password) && !isEmpty(confirm))
+        if(!isEmpty(email) && !isEmpty(password) && !isEmpty(confirm) && !isEmpty(name))
         {
             if(isEmailValid(email.getText().toString()))
             {
                 if(confirm.getText().toString().equals(password.getText().toString()))
                 {
-                    try
+                    if(isAlpha(name.getText().toString()))
                     {
-                        String save = email.getText().toString() + ";" + password.getText().toString() + ";";
-                        FileOutputStream fileOut = openFileOutput("save.txt", MODE_PRIVATE);
-                        OutputStreamWriter outputWriter = new OutputStreamWriter(fileOut);
-                        outputWriter.write(save);
-                        outputWriter.close();
-                        ShowMessage("Done", true);
+                        try
+                        {
+                            String save = email.getText().toString() + ";" + password.getText().toString() + ";";
+                            FileOutputStream fileOut = openFileOutput("save.txt", MODE_PRIVATE);
+                            OutputStreamWriter outputWriter = new OutputStreamWriter(fileOut);
+                            outputWriter.write(save);
+                            outputWriter.close();
+                            ShowMessage("Done", true);
+                        }
+                        catch (Exception e)
+                        {
+                            ShowMessage("Error!", false);
+                        }
                     }
-                    catch (Exception e)
+                    else
                     {
-                        ShowMessage("Error!", false);
+                        ShowMessage("Insert a Name",false);
                     }
                 }
                 else
@@ -195,7 +358,8 @@ public class MainActivity extends AppCompatActivity {
             {
                 try
                 {
-                    FileInputStream fileIn = openFileInput("save.txt");
+                    Login(email.getText().toString(), password.getText().toString());
+                    /*FileInputStream fileIn = openFileInput("save.txt");
                     InputStreamReader isr = new InputStreamReader(fileIn);
                     BufferedReader bufferedReader = new BufferedReader(isr);
                     StringBuilder sb = new StringBuilder();
@@ -233,7 +397,7 @@ public class MainActivity extends AppCompatActivity {
                         {
                             ShowMessage("Email not registered", false);
                         }
-                    }
+                    }*/
                 }
                 catch (Exception e){ShowMessage("Error!", false);}
             }
@@ -245,6 +409,17 @@ public class MainActivity extends AppCompatActivity {
         {
             ShowMessage("Fill All", false);
         }
+    }
+    public boolean isAlpha(String name) {
+        char[] chars = name.toCharArray();
+
+        for (char c : chars) {
+            if(!Character.isLetter(c)) {
+                return false;
+            }
+        }
+
+        return true;
     }
     private static final String[] OPCOES = {"Carregar Imagens",	"Exibir Imagens"	};
     private static final String[] ACOES = {"TELA_IMAGEM", "TELA_EXI"};
